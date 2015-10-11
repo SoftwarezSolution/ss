a9933044b809d8a77f75050290e675ca776965e9
Nilay Jayswal <nilayjayswalnj@gmail.com>
2015-10-12 00:01:20 +0530

    commit
    Changes done!!!!

--- blog/form.php
+++ blog/form.php
@@ -17,7 +17,7 @@ else{
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Edmin</title>
+	<title>| Document |</title>
 	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 	<link type="text/css" href="css/theme.css" rel="stylesheet">
@@ -49,56 +49,10 @@ else{
 				</a>
 
 			  	<a class="brand" href="index.php">
-			  		Edmin
+			  		Softwarez Solutions : Admin
 			  	</a>
-
-				<div class="nav-collapse collapse navbar-inverse-collapse">
-					<ul class="nav nav-icons">
-						<li class="active"><a href="#">
-							<i class="icon-envelope"></i>
-						</a></li>
-						<li><a href="#">
-							<i class="icon-eye-open"></i>
-						</a></li>
-						<li><a href="#">
-							<i class="icon-bar-chart"></i>
-						</a></li>
-					</ul>
-
-					<form class="navbar-search pull-left input-append" action="#">
-						<input type="text" class="span3">
-						<button class="btn" type="button">
-							<i class="icon-search"></i>
-						</button>
-					</form>
-				
-					<ul class="nav pull-right">
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Item No. 1</a></li>
-								
-								<li><a href="#">Don't Click</a></li>
-								<li class="divider"></li>
-								<li class="nav-header">Example Header</li>
-								<li><a href="#">A Separated link</a></li>
-															</ul>
-						</li>
-						
-						<li><a href="#">
-							Support
-						</a></li>
-						<li class="nav-user dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-								<img src="images/user.png" class="nav-avatar" />
-								<b class="caret"></b>
-							</a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Your Profile</a></li>
-								<li><a href="#">Edit Profile</a></li>
-								<li><a href="#">Account Settings</a></li>
-								<li class="divider"></li>
-								<li><a href="#">Logout</a></li>
+
+					<ul class="nav pull-right">					<li><a href="#">Logout</a></li>
 							</ul>
 						</li>
 					</ul>
@@ -112,94 +66,7 @@ else{
 	<div class="wrapper">
 		<div class="container">
 			<div class="row">
-				<div class="span3">
-					<div class="sidebar">
-
-						<ul class="widget widget-menu unstyled">
-							<li class="active">
-								<a href="index.php">
-									<i class="menu-icon icon-dashboard"></i>
-									Dashboard
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<i class="menu-icon icon-bullhorn"></i>
-									News Feed
-								</a>
-							</li>
-							<li>
-								<a href="#">
-									<i class="menu-icon icon-inbox"></i>
-									Inbox
-									<b class="label green pull-right">11</b>
-								</a>
-							</li>
-							
-							<li>
-								<a href="#">
-									<i class="menu-icon icon-tasks"></i>
-									Tasks
-									<b class="label orange pull-right">19</b>
-								</a>
-							</li>
-						</ul><!--/.widget-nav-->
-
-						<ul class="widget widget-menu unstyled">
-                                <li><a href="#"><i class="menu-icon icon-bold"></i> Buttons </a></li>
-                                <li><a href="#"><i class="menu-icon icon-book"></i>Typography </a></li>
-                                <li><a href="form.php"><i class="menu-icon icon-paste"></i>Add Post </a></li>
-                                <li><a href="table.php"><i class="menu-icon icon-table"></i>Tables </a></li>
-                                <li><a href="#"><i class="menu-icon icon-bar-chart"></i>Charts </a></li>
-                            </ul><!--/.widget-nav-->
-
-						<ul class="widget widget-menu unstyled">
-							<li>
-								<a class="collapsed" data-toggle="collapse" href="#togglePages">
-									<i class="menu-icon icon-cog"></i>
-									<i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right"></i>
-									More Pages
-								</a>
-								<ul id="togglePages" class="collapse unstyled">
-									<li>
-										<a href="#">
-											<i class="icon-inbox"></i>
-											Login
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="icon-inbox"></i>
-											Profile
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<i class="icon-inbox"></i>
-											All Users
-										</a>
-									</li>
-								</ul>
-							</li>
-							
-							<li>
-								<a href="includes/logout.php">
-									<i class="menu-icon icon-signout"></i>
-									Logout
-								</a>
-							</li>
-						</ul>
-
-					</div><!--/.sidebar-->
-				</div><!--/.span3-->
-
-				
-				
-				
-				
-				
-
-				<div class="span9">
+				<div class="span12">
 					<div class="content">
 
 						<div class="module">
@@ -311,7 +178,7 @@ else{
 	<div class="footer">
 		<div class="container">
 			 
-			<b class="copyright">&copy; 2014 Edmin - EGrappler.com </b> All rights reserved.
+			<b class="copyright">&copy; Admin - naipathya.in </b> All rights reserved.
 		</div>
 	</div>
 
--- blog/welcome.php
+++ blog/welcome.php
@@ -14,7 +14,7 @@ else{
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Admin naipathya</title>
+        <title>| Document |</title>
         <link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
         <link type="text/css" href="css/theme.css" rel="stylesheet">
@@ -27,40 +27,10 @@ else{
             <div class="navbar-inner">
                 <div class="container">
                     <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                        <i class="icon-reorder shaded"></i></a><a class="brand" href="welcome.php">Admin </a>
-                    <div class="nav-collapse collapse navbar-inverse-collapse">
-                        <ul class="nav nav-icons">
-                            <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
-                            <li><a href="#"><i class="icon-eye-open"></i></a></li>
-                            <li><a href="#"><i class="icon-bar-chart"></i></a></li>
-                        </ul>
-                        <form class="navbar-search pull-left input-append" action="#">
-                        <input type="text" class="span3">
-                        <button class="btn" type="button">
-                            <i class="icon-search"></i>
-                        </button>
-                        </form>
-                        <ul class="nav pull-right">
-                            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
-                                <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Item No. 1</a></li>
-                                    <li><a href="#">Don't Click</a></li>
-                                    <li class="divider"></li>
-                                    <li class="nav-header">Example Header</li>
-                                    <li><a href="#">A Separated link</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Support </a></li>
-                            <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="images/user.png" class="nav-avatar" />
-                                <b class="caret"></b></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Your Profile</a></li>
-                                    <li><a href="#">Edit Profile</a></li>
-                                    <li><a href="#">Account Settings</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="includes/logout.php">Logout</a></li>
+                        <i class="icon-reorder shaded"></i></a><a class="brand" href="welcome.php">Softwarez Solutions : Admin </a>
+                    <div class="nav-collapse collapse navbar-inverse-collapse">
+                      
+                        <ul class="nav pull-right">						 <li><a href="includes/logout.php">Log Out</a></li>
                                 </ul>
                             </li>
                         </ul>
@@ -74,58 +44,12 @@ else{
         <div class="wrapper">
             <div class="container">
                 <div class="row">
-                    <div class="span3">
-                        <div class="sidebar">
-                            <ul class="widget widget-menu unstyled">
-                                <li class="active"><a href="welcome.php"><i class="menu-icon icon-dashboard"></i>Dashboard
-                                </a></li>
-                                <li><a href="#"><i class="menu-icon icon-bullhorn"></i>News Feed </a>
-                                </li>
-                                <li><a href="#"><i class="menu-icon icon-inbox"></i>Inbox <b class="label green pull-right">
-                                    11</b> </a></li>
-                                <li><a href="#"><i class="menu-icon icon-tasks"></i>Tasks <b class="label orange pull-right">
-                                    19</b> </a></li>
-                            </ul>
-                            <!--/.widget-nav-->
-                            
-                            
-                            <ul class="widget widget-menu unstyled">
-                                <li><a href="#"><i class="menu-icon icon-bold"></i> Buttons </a></li>
-                                <li><a href="#"><i class="menu-icon icon-book"></i>Typography </a></li>
-                                <li><a href="form.php"><i class="menu-icon icon-paste"></i>Forms </a></li>
-                                <li><a href="table.php"><i class="menu-icon icon-table"></i>Tables </a></li>
-                                <li><a href="#"><i class="menu-icon icon-bar-chart"></i>Charts </a></li>
-                            </ul>
-                            <!--/.widget-nav-->
-                            <ul class="widget widget-menu unstyled">
-                                <li><a class="collapsed" data-toggle="collapse" href="#togglePages"><i class="menu-icon icon-cog">
-                                </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
-                                </i>More Pages </a>
-                                    <ul id="togglePages" class="collapse unstyled">
-                                        <li><a href="#"><i class="icon-inbox"></i>Login </a></li>
-                                        <li><a href="#"><i class="icon-inbox"></i>Profile </a></li>
-                                        <li><a href="#"><i class="icon-inbox"></i>All Users </a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="includes/logout.php"><i class="menu-icon icon-signout"></i>Logout </a></li>
-                            </ul>
-                        </div>
-                        <!--/.sidebar-->
-                    </div>
-                    <!--/.span3-->
-                    <div class="span9">
+                    <div class="span12">
                         <div class="content">
                             <div class="btn-controls">
-                                <div class="btn-box-row row-fluid">
-                                    <a href="#" class="btn-box big span4"><i class=" icon-random"></i><b>65%</b>
-                                        <p class="text-muted">
-                                            Growth</p>
-                                    </a><a href="#" class="btn-box big span4"><i class="icon-user"></i><b>15</b>
-                                        <p class="text-muted">
-                                            New Users</p>
-                                    </a><a href="#" class="btn-box big span4"><b>Upload File and get url</b>
-                                        <p class="text-muted">
-                                                 
+                                <div class="btn-box-row row-fluid">																<a href="form.php" class="btn-box small span4"><i class="icon-envelope"></i><b>Add new post</b></a>                                   <a href="table.php" class="btn-box small span4"><i class="icon-group"></i><b>All Posts</b></a>                                       
+                                   <a href="#" class="btn-box big span4"><b>Upload File and get url</b>
+                                        <p class="text-muted">
                                                   <form method="post" enctype="multipart/form-data">
                                              <input name="uploadedfile" type="file" >
                                               <input type="submit" value="Upload" name="submit">
@@ -150,90 +74,7 @@ url:<a href=" uploads/<?php echo $_FILES['uploadedfile']['name'];  ?>" target="_
 
                                             </p>
                                     </a>
-                                </div>
-                                
-
-                                <div class="btn-box-row row-fluid">
-                                    <div class="span8">
-                                        <div class="row-fluid">
-                                            <div class="span12">
-                                                <a href="form.php" class="btn-box small span4"><i class="icon-envelope"></i><b>Add new post</b>
-                                                </a><a href="table.php" class="btn-box small span4"><i class="icon-group"></i><b>All Posts</b>
-                                                </a><a href="#" class="btn-box small span4"><i class="icon-exchange"></i><b>Add User</b>
-                                                </a>
-                                            </div>
-                                        </div>
-                                        <div class="row-fluid">
-                                            <div class="span12">
-                                                <a href="#" class="btn-box small span4"><i class="icon-save"></i><b>All user</b>
-                                                </a><a href="#" class="btn-box small span4"><i class="icon-bullhorn"></i><b>Social Feed</b>
-                                                </a><a href="#" class="btn-box small span4"><i class="icon-sort-down"></i><b>Bounce
-                                                    Rate</b> </a>
-                                            </div>
-                                        </div>
- 
-                                    </div>
-
- 
- 
-                                    <ul class="widget widget-usage unstyled span4">
-                                        <li>
-                                            <p>
-                                                <strong>Windows 8</strong> <span class="pull-right small muted">78%</span>
-                                            </p>
-                                            <div class="progress tight">
-                                                <div class="bar" style="width: 78%;">
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <p>
-                                                <strong>Mac</strong> <span class="pull-right small muted">56%</span>
-                                            </p>
-                                            <div class="progress tight">
-                                                <div class="bar bar-success" style="width: 56%;">
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <p>
-                                                <strong>Linux</strong> <span class="pull-right small muted">44%</span>
-                                            </p>
-                                            <div class="progress tight">
-                                                <div class="bar bar-warning" style="width: 44%;">
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <p>
-                                                <strong>iPhone</strong> <span class="pull-right small muted">67%</span>
-                                            </p>
-                                            <div class="progress tight">
-                                                <div class="bar bar-danger" style="width: 67%;">
-                                                </div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <!--/#btn-controls-->
-                    <!--        
-                            <div class="module">
-                                <div class="module-head">
-                                    <h3>
-                                        Profit Chart</h3>
-                                </div>
-                                <div class="module-body">
-                                    <div class="chart inline-legend grid">
-                                        <div id="placeholder2" class="graph" style="height: 500px">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        -->     
-                        
-                        
-                            <!--/.module-->
+                                </div>
                             <div class="module hide">
                                 <div class="module-head">
                                     <h3>
@@ -356,7 +197,7 @@ url:<a href=" uploads/<?php echo $_FILES['uploadedfile']['name'];  ?>" target="_
         <!--/.wrapper-->
         <div class="footer">
             <div class="container">
-                <b class="copyright">&copy; 2014 Edmin - naipathya.in </b>All rights reserved.
+                <b class="copyright">&copy; 2014 Admin - naipathya.in </b>All rights reserved.
             </div>
         </div>
         <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
