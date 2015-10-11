cae03b9128bdfcf7af22d0d3c5c460a5e3b2dc57
Nilay Jayswal <nilayjayswalnj@gmail.com>
2015-10-11 16:07:47 +0530

    commit
    PUSH

---
+++ User/index.php
@@ -0,0 +1,129 @@
+﻿
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>| Document |</title>
+    <link href=" css/bootstrap.css" rel="stylesheet" />
+    <link href=" css/font-awesome.css" rel="stylesheet" />
+    <link href=" js/morris/morris-0.4.3.min.css" rel="stylesheet" />
+    <link href=" css/custom-styles.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default top-navbar" role="navigation">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+				<h4>&nbsp;&nbsp;&nbsp;Hi! <?php echo $_SESSION['email']; ?></h4>
+            </div>
+
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fa fa-bell fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-alerts">
+                        <li>
+                            <a href="#">
+                                <div>
+                                    <i class="fa fa-comment fa-fw"></i>Notification
+                                    <span class="pull-right text-muted small">4 min</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#">
+                                <div>
+                                    <i class="fa fa-comment fa-fw"></i>Notification
+                                    <span class="pull-right text-muted small">34 min</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-home fa-fw"></i> Softwarez Solutions : Home</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Support</a>
+                        </li>
+						<li><a href="#"><i class="fa fa-pencil fa-fw"></i> Blogs</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a class="active-menu" href="index-2.html"><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="form.html"><i class="fa fa-edit"></i>Fill-in-Form </a>
+                    </li>
+					<li>
+                        <a href="newchat/start.html"><i class="fa fa-user"></i>Chat with us</a>
+                    </li>
+					<li>
+                        <a href="https://www.payumoney.com/paybypayumoney/#/50793"><i class="fa fa-money"></i>Pay with Pay U Money</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+
+
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h1 class="page-header">
+                            Welcome to Softwarez Solutions
+                        </h1>
+                    </div>
+                </div>
+				
+				<div class="row">
+				<div class="col-lg-12">
+                                    <form action="" method="">
+										<div class="form-group">
+                                            <label>Leave your message here :</label>
+                                            <textarea class="form-control" rows="3" name="message"></textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-default">Submit</button>
+                                    </form>
+                                </div>
+								</div>
+            </div>
+        </div>
+    </div>
+	
+	<script src=" js/jquery-1.10.2.js"></script>
+    <script src=" js/bootstrap.min.js"></script>
+    <script src=" js/jquery.metisMenu.js"></script>
+    <script src=" js/custom-scripts.js"></script>
+	
+
+</body>
+</html>
